7daf0e6f7685ddf2a106c2fc73e29928f8345f44
Strexas <kirsnauskas@bk.ru>
2021-02-10 06:35:07 +0300

    комментарии, вынесение fps

--- scripts/start.py
+++ scripts/start.py
@@ -1,15 +1,16 @@
 import pygame
 
 import os
-from main import Main
-os.chdir('..')
+from main import Main  # класс отвечающий за игру
+os.chdir('..')  # для удобства вернулись на папку выше
 pygame.init()
-display = pygame.display.set_mode((1000, 700))
+display = pygame.display.set_mode((1000, 700))  # создали игровое окно
 clock = pygame.time.Clock()
-game = Main(display)
+fps = 60  # fps игры
+game = Main(display, fps)  # передали его на изменения в класс Main
 
 
 while True:
-    game.event_handler(pygame.event.get())
-    game.draw()
-    clock.tick(60)
+    game.event_handler(pygame.event.get())  # перехватчик событий
+    game.draw()  # отрисовка
+    clock.tick(fps)  # FPS
