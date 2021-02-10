bd90ba59b6f2dfcc786ff9a4135e453c97f0bd9b
Strexas <kirsnauskas@bk.ru>
2021-02-10 06:37:17 +0300

    повышение читаемости кода, создание общего класса Menu, сокращение кода

--- scripts/menu.py
+++ scripts/menu.py
@@ -1,70 +1,47 @@
 import pygame
 
 
-class Game_Menu:
-    def __init__(self, width: int, height: int,
-                 objects: dict, font_size: int,
-                 menu_background_color=pygame.Color((0, 0, 0)),
-                 menu_rect_color=pygame.Color((100, 190, 85)),
-                 text_color=pygame.Color((255, 255, 255)),
-                 text_color_focused=pygame.Color((4, 90, 200)), margin=25, alpha=125):  # alpha?
-        self.functions = list(objects.values())
-
+class Menu:
+    def __init__(self, width: int, height: int, objects: dict, font_size: int):
         self.width = width
         self.height = height
-        self.margin = margin
+        self.margin = 0
 
-        self.menu_bg_color = menu_background_color
-        self.menu_rect_color = menu_rect_color
-        self.label_color = text_color
-        self.label_color_focused = text_color_focused
+        self.functions = list(objects.values())
+        self.texts = list(objects.keys())
+
+        self.label_color = pygame.Color((255, 255, 255))
+        self.label_color_focused = pygame.Color((4, 90, 200))
+        self.bg_color = pygame.Color((100, 190, 85))
+
+        self.font = pygame.font.Font('data/fonts/19255.ttf', font_size)
 
         self.surface = pygame.Surface((self.width, self.height))
-        self.surface_alpha = alpha
-        self.surface.set_alpha(self.surface_alpha)
 
-        self.texts = list(objects.keys())
-        self.font = pygame.font.Font('data/fonts/19255.ttf', font_size)
-        self.font_height = font_size
-
-        self.menu_width = max([self.font.size(i)[0]
-                               for i in self.texts]) + self.margin * 2
-
-        self.menu_height = len(self.texts) * self.font_height + \
-            self.margin * (len(self.texts) + 2)
-
-        self.menu_rect = pygame.rect.Rect(self.width // 2 - self.menu_width // 2,
-                                          self.height // 2 - self.menu_height // 2,
-                                          self.menu_width, self.menu_height)
-
-        self.label_rects = self.generate_label_rects()
-
-        self.rendered_labels = [
-            self.font.render(i, True, self.label_color)
-            for i in self.texts]
-
-        self.rendered_labels_focused = [
-            self.font.render(i, True, self.label_color_focused)
-            for i in self.texts]
-
-        for x, y in zip(self.rendered_labels, self.rendered_labels_focused):
-            x.set_alpha(self.surface_alpha)
-            y.set_alpha(self.surface_alpha)
-
-        self.timer = pygame.time.get_ticks()
-
-    def generate_label_rects(self):
-        y = self.menu_rect.y + self.margin
-
-        rects = []
+        self.rendered_labels = []
+        self.rendered_labels_focused = []
 
         for i in self.texts:
-            rects.append(pygame.rect.Rect(
-                (self.width // 2 - self.font.size(i)[0] // 2, y),
-                self.font.size(i)))
+            self.rendered_labels.append(self.font.render(i, True, self.label_color))
+            self.rendered_labels_focused.append(self.font.render(i, True, self.label_color_focused))
 
-            y += self.margin + self.font_height
-        return rects
+        self.label_rects = []
+
+    def generate_label_rects(self, y):
+        y = y
+
+        text_rects = []
+
+        for i in self.texts:
+            rect = pygame.rect.Rect(
+                ((self.width - self.font.size(i)[0]) // 2, y),
+                self.font.size(i))
+
+            text_rects.append(rect)
+
+            y += self.margin + self.font.size(i)[1]
+
+        return text_rects
 
     def draw_text(self):
         mx, my = pygame.mouse.get_pos()
@@ -76,16 +53,40 @@ class Game_Menu:
 
                 self.surface.blit(self.rendered_labels_focused[i], self.label_rects[i])
 
-                if m1 and self.timer + 500 < pygame.time.get_ticks():
-                    self.timer = pygame.time.get_ticks()
+                if m1:
                     self.functions[i]()
-
             else:
                 self.surface.blit(self.rendered_labels[i], self.label_rects[i])
 
+
+class Game_Menu(Menu):
+    def __init__(self, width: int, height: int, objects: dict, font_size: int):
+        super().__init__(width, height, objects, font_size)
+
+        self.margin = 25
+
+        self.surface_alpha = 225
+        self.surface.set_alpha(self.surface_alpha)
+
+        self.font_height = font_size
+
+        self.menu_width = self.font.size(max(self.texts))[0] + self.margin * 2
+
+        self.menu_height = len(self.texts) * (self.font_height + self.margin) + self.margin
+
+        self.menu_rect = pygame.rect.Rect((self.width - self.menu_width) // 2,
+                                          (self.height - self.menu_height) // 2,
+                                          self.menu_width, self.menu_height)
+
+        self.label_rects = self.generate_label_rects(self.menu_rect.y + self.margin)
+
+        for x, y in zip(self.rendered_labels, self.rendered_labels_focused):
+            x.set_alpha(self.surface_alpha)
+            y.set_alpha(self.surface_alpha)
+
     def render(self):
         pygame.draw.rect(self.surface,
-                         self.menu_rect_color,
+                         self.bg_color,
                          self.menu_rect, border_radius=7)
 
         self.draw_text()
@@ -93,85 +94,34 @@ class Game_Menu:
         return self.surface
 
 
-class Main_Menu:
-    def __init__(self, width, height,
-                 objects: dict, game_name: str,
-                 font_size: int, bg_color: pygame.Color,
-                 margin=35, bottom_margin=150,
-                 text_color=pygame.Color((255, 255, 255)),
-                 text_color_focused=pygame.Color((4, 90, 200))):
+class Main_Menu(Menu):
+    def __init__(self, width, height, objects: dict, font_size: int):
 
-        self.functions = list(objects.values())
+        super().__init__(width, height, objects, font_size)
 
-        self.width = width
-        self.height = height
-
-        self.surface = pygame.Surface((self.width, self.height))
-
-        self.texts = list(objects.keys())
-        self.text_font = pygame.font.Font('data/fonts/19255.ttf', font_size)
-        self.name_font = pygame.font.SysFont('Arial', int(font_size * 1.5), True)
+        self.name_font = pygame.font.SysFont('Arial', int(font_size * 2), True)
 
         self.name_fs = int(font_size * 1.25)
-        self.margin = margin
-        self.bottom_margin = bottom_margin
+        self.margin = 35
+        self.bottom_margin = 150
 
-        self.game_name = game_name
-
-        self.label_color = text_color
-        self.label_color_focused = text_color_focused
-        self.bg_color = bg_color
+        self.game_name = 'Racer'
 
         self.rendered_name = self.name_font.render(self.game_name, True, self.label_color)
 
-        self.rendered_labels = [
-            self.text_font.render(i, True, self.label_color)
-            for i in self.texts]
+        self.label_rects = self.generate_label_rects(self.margin * 4 + self.rendered_name.get_height())
 
-        self.rendered_labels_focused = [
-            self.text_font.render(i, True, self.label_color_focused)
-            for i in self.texts]
+        self.draw_text = self.draw_name(self.draw_text)
 
-        self.generate_text_rect()
-        self.timer = pygame.time.get_ticks()
-
-    def generate_text_rect(self):
-        y = self.height - self.bottom_margin
-
-        self.text_rects = []
-
-        for i in self.rendered_labels[::-1]:
-            rect = pygame.rect.Rect(
-                self.width // 2 - i.get_width() // 2,
-                y, i.get_width(), i.get_height())
-
-            self.text_rects.append(rect)
-
-            y -= self.margin + i.get_height()
-
-        self.text_rects = self.text_rects[::-1]
-
-        self.name_rect = pygame.rect.Rect(
-            self.width // 2 - self.rendered_name.get_width() // 2,
-            y - 50, self.rendered_name.get_width(),
-            self.rendered_name.get_height())
-
-    def draw_text(self):
-        mx, my = pygame.mouse.get_pos()
-
-        m1, m2, m3 = pygame.mouse.get_pressed()
-
-        self.surface.blit(self.rendered_name, self.name_rect)
-
-        for i in range(len(self.texts)):
-            if self.text_rects[i].collidepoint(mx, my):
-
-                self.surface.blit(self.rendered_labels_focused[i], self.text_rects[i])
-
-                if m1:
-                    self.functions[i]()
-            else:
-                self.surface.blit(self.rendered_labels[i], self.text_rects[i])
+    def draw_name(self, func):
+        def decorated_draw():
+            func()
+            self.name_rect = pygame.rect.Rect(
+                (self.width - self.rendered_name.get_width()) // 2,
+                self.margin * 2, self.rendered_name.get_width(),
+                self.rendered_name.get_height())
+            self.surface.blit(self.rendered_name, self.name_rect)
+        return decorated_draw
 
     def render(self):
         self.surface.fill(self.bg_color)
