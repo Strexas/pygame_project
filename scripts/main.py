59acf95a2542adce7b85513c3137f3f7d959d030
Strexas <kirsnauskas@bk.ru>
2021-02-10 20:44:56 +0300

    Добавление фоновой музыки, сокращение кода, комментарии

--- scripts/main.py
+++ scripts/main.py
@@ -9,8 +9,9 @@ class Main:
         self.resolution = display.get_size()
         self.FPS = fps
         self.speed = 0
-        self.status = 'main_menu'
+        self.status = 'main_menu'  # отслеживаем текущее положение
 
+        # создание Главного меню
         self.main_menu = Main_Menu(
             self.resolution[0], self.resolution[1],
 
@@ -20,7 +21,7 @@ class Main:
              'Выход': self.exit},
 
             self.resolution[0] // 100 * 5)
-
+        # создание Игрового меню
         self.game_menu = Game_Menu(
             self.resolution[0], self.resolution[1],
 
@@ -32,27 +33,27 @@ class Main:
         self.SPEEDUPEVENT = pygame.USEREVENT + 1
         self.GAMEOVEREVENT = pygame.USEREVENT + 2
 
+    def new_game(self):  # начало игры
+        self.game = Game(self.resolution[0], self.resolution[1])
+        pygame.time.set_timer(self.SPEEDUPEVENT, 100)
+        self.status = 'game'
+
     def back(self):
         self.status = 'main_menu'
 
     def resume(self):
         self.status = 'game'
 
-    def new_game(self):
-        self.game = Game(self.resolution[0], self.resolution[1])
-        pygame.time.set_timer(self.SPEEDUPEVENT, 100)
-        self.status = 'game'
-
-    def game_info(self):
+    def authors(self):
         pass
 
-    def authors(self):
+    def game_info(self):
         pass
 
     def exit(self):
         exit()
 
-    def event_handler(self, events):
+    def event_handler(self, events):  # перехватчик событий
         for i in events:
             if i.type == pygame.QUIT:
                 self.exit()
@@ -60,26 +61,22 @@ class Main:
             if i.type == pygame.KEYDOWN:
                 self.keyboard_handler(pygame.key.get_pressed())
 
-            if i.type == self.SPEEDUPEVENT and self.status == 'game':
+            if i.type == self.SPEEDUPEVENT and self.status == 'game':  # ускорение игры
                 self.game.speed_up()
                 if self.game.speed > 50:
-                    pygame.time.set_timer(pygame.USEREVENT + 1, 0)
+                    self.game.speed = 50
+                    pygame.time.set_timer(pygame.USEREVENT + 1, 0)  # перестаем ускорять игру
 
-            if i.type == self.GAMEOVEREVENT:
+            if i.type == self.GAMEOVEREVENT:  # выход из игры
                 self.status = 'main_menu'
 
-    def keyboard_handler(self, keys):
-        escape_pressed = keys[pygame.K_ESCAPE]
-
-        if self.status == 'game' and escape_pressed:
-            self.status = 'game_menu'
+    def keyboard_handler(self, keys):  # переход от игрового меню к игре и обратно
+        if not (keys[pygame.K_ESCAPE] and 'game' in self.status):
             return
 
-        if self.status == 'game_menu' and escape_pressed:
-            self.status = 'game'
-            return
+        self.status = 'game_menu' if self.status == 'game' else 'game'
 
-    def draw(self):
+    def draw(self):  # отрисовка в зависимости от текущего положения
         self.display.fill((0, 0, 0))
         if self.status == 'main_menu':
             self.display.blit(self.main_menu.render(), (0, 0))
--- scripts/start.py
+++ scripts/start.py
@@ -1,4 +1,5 @@
 import pygame
+from random import choice
 
 import os
 from main import Main  # класс отвечающий за игру
@@ -8,7 +9,8 @@ display = pygame.display.set_mode((1000, 700))  # создали игровое 
 clock = pygame.time.Clock()
 fps = 60  # fps игры
 game = Main(display, fps)  # передали его на изменения в класс Main
-
+songs = [pygame.mixer.Sound(f'data/music/{i}') for i in os.listdir('data/music/')]  # загрузили треки
+choice(songs).play()  # проигрываем случайный
 
 while True:
     game.event_handler(pygame.event.get())  # перехватчик событий
